1f4f3d611455577ab476e3a1b85ab3ee63acc819
Navjot Singh <weavebytes@gmail.com>
2017-12-29 13:11:59 +0530

    updated event

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,6 @@
 const express      = require('express');
 const router       = express.Router();
-const Event    = require('../schema/event');
+const Event        = require('../schema/event');
 
 
 //--------------------------------------------------
